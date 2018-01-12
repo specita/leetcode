1522b0f99aac35a646c3f16170af375fc8565d8e
Cocoon <cocoon@codoondeMacBook-Pro-5.local>
2018-01-12 15:27:03 +0800

    二分查找

---
+++ SearchInsertPosition/solution.go
@@ -0,0 +1,43 @@
+package SearchInsertPosition
+
+//Given a sorted array and a target value, return the index if the target is found. If not,
+//return the index where it would be if it were inserted in order.
+//
+//You may assume no duplicates in the array.
+//
+//Example 1:
+//
+//Input: [1,3,5,6], 5
+//Output: 2
+//Example 2:
+//
+//Input: [1,3,5,6], 2
+//Output: 1
+//Example 3:
+//
+//Input: [1,3,5,6], 7
+//Output: 4
+//Example 1:
+//
+//Input: [1,3,5,6], 0
+//Output: 0
+
+func SearchInsert(nums []int, target int) int {
+	return binarySearch(nums, 0, len(nums)-1, target)
+}
+
+func binarySearch(nums []int, start, end, target int) int {
+	if start > end {
+		return end + 1
+	}
+
+	mid := (start + end) / 2
+
+	if nums[mid] == target {
+		return mid
+	} else if nums[mid] > target {
+		return binarySearch(nums, start, mid-1, target)
+	} else {
+		return binarySearch(nums, mid+1, end, target)
+	}
+}
---
+++ SearchInsertPosition/solution_test.go
@@ -0,0 +1,24 @@
+package SearchInsertPosition
+
+import "testing"
+
+func TestSearchInsert(t *testing.T) {
+	table := []struct {
+		nums   []int
+		target int
+		expect int
+	}{
+		{[]int{1, 3, 5, 6}, 5, 2},
+		{[]int{1, 3, 5, 6}, 3, 1},
+		{[]int{1, 3, 5, 6}, 7, 4},
+		{[]int{1, 3, 5, 6}, 0, 0},
+	}
+
+	for _, e := range table {
+		output := SearchInsert(e.nums, e.target)
+		if output != e.expect {
+			t.Fatalf("case: %v, output: %v", e, output)
+		}
+	}
+
+}
